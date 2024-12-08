7ff2e0b868de7e8b505deeb1ff3196847126b353
shkwlx <aleks.dudin2018@gmail.com>
2024-12-08 17:01:55 +0200

    Дз Паршина

---
+++ дз c#.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a < b)
+        {
+            Console.WriteLine("Числа в порядке возрастания:");
+            for (int i = a; i <= b; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Числа в порядке убывания:");
+            for (int i = a; i >= b; i--)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
